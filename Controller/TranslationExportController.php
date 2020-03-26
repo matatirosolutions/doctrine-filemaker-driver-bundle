3516ed8f15ce709152e4b84fbd976dff1532be07
Steve Winter <steve@msdev.co.uk>
2020-03-26 12:24:34 +0000

    Additional explicit requirement

--- Controller/TranslationExportController.php
+++ Controller/TranslationExportController.php
@@ -2,20 +2,21 @@
 
 namespace MSDev\DoctrineFileMakerDriverBundle\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
+
 
 class TranslationExportController extends AbstractController
 {
-
     public function TranslationExportAction()
     {
+        /** @var KernelInterface $kernel */
         $kernel = $this->get('kernel');
         $application = new Application($kernel);
         $application->setAutoExit(false);
@@ -27,10 +28,16 @@ class TranslationExportController extends AbstractController
             OutputInterface::VERBOSITY_NORMAL,
             true
         );
-        $application->run($input, $output);
-
-        return new JsonResponse([
-            'success' => true
-        ]);
+        try {
+            $application->run($input, $output);
+            return new JsonResponse([
+                'success' => true
+            ]);
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
