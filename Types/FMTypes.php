4c4ad5a7e6f848001444cf4f23b660b80307b494
Steve Winter <steve@msdev.nz>
2026-08-28 12:26:09 +1200

    Add constants for better type mapping

---
+++ Types/FMTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MSDev\DoctrineFileMakerDriverBundle\Types;
+
+final class FMTypes
+{
+    public const DATE = 'fmdate';
+    public const DATETIME = 'fmdatetime';
+    public const TIME = 'fmtime';
+    public const HTML = 'fmhtml';
+    public const FLOAT = 'fmfloat';
+    public const INT = 'fmint';
+}
